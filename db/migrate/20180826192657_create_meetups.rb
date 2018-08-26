97219ef2b170431a7f2a0c4dac9b6fe7f1c265cb
kduane <karl.duane@gmail.com>
2018-08-26 17:43:16 -0400

    migration not working, changing machines

--- db/migrate/20180826192657_create_meetups.rb
+++ db/migrate/20180826192657_create_meetups.rb
@@ -3,6 +3,7 @@ class CreateMeetups < ActiveRecord::Migration[5.2]
     create table :meetups do |t|
       t.string :name, null: false
       t.string :description, null: false
+
     end
   end
 end
--- db/migrate/20180826193744_create_signups.rb
+++ db/migrate/20180826193744_create_signups.rb
@@ -3,6 +3,7 @@ class CreateSignups < ActiveRecord::Migration[5.2]
     create_table :signups do |t|
       t.belongs_to :meetup
       t.belongs_to :user
+
     end
   end
 end
