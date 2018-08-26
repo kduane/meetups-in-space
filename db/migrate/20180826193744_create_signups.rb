3b85a6c7c994843bae4167eeba4d4afaed7847ff
kduane <karl.duane@gmail.com>
2018-08-26 17:41:16 -0400

    prepped for first migration

--- db/migrate/20180826193744_create_signups.rb
+++ db/migrate/20180826193744_create_signups.rb
@@ -1,9 +1,8 @@
 class CreateSignups < ActiveRecord::Migration[5.2]
   def change
     create_table :signups do |t|
-      t.string :name, null: false
-
-      t.timestamps, null: false
+      t.belongs_to :meetup
+      t.belongs_to :user
     end
   end
 end
