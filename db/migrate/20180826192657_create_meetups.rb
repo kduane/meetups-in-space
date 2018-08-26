529963d6647993ef88eb1d34f38550cdf297f29c
kduane <karl.duane@gmail.com>
2018-08-26 17:51:22 -0400

    renamed description to details

--- db/migrate/20180826192657_create_meetups.rb
+++ db/migrate/20180826192657_create_meetups.rb
@@ -2,7 +2,7 @@ class CreateMeetups < ActiveRecord::Migration[5.2]
   def change
     create table :meetups do |t|
       t.string :name, null: false
-      t.string :description, null: false
+      t.string :details, null: false
 
     end
   end
